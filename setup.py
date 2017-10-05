9781b415932aebeea50c5ad5f291b0f7abab896a
Diorge Brognara <diorge.bs@gmail.com>
2017-10-05 15:42:35 -0300

    Basic project structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+DESCRIPTION = "Implementation of the Bayesian Sets algorithm and variants"
+
+setup(
+    name="BayesSets",
+    version="0.1",
+    packages=["bayessets"],
+    author="Diorge Brognara",
+    author_email="diorge.bs@gmail.com",
+    description=DESCRIPTION,
+    license="MIT",
+    keywords="bayes set expansion machine learning information retrieval",
+    url="https://github.com/MaLL-UFSCar/bayessets"
+)
