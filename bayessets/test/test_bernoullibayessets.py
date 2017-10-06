93457a00f5806f2425603c6077ba1a7d8ee4c27f
Diorge Brognara <diorge.bs@gmail.com>
2017-10-06 18:55:14 -0300

    Unit test for checking the mean scale argument is not assumed to be 2.

--- bayessets/test/test_bernoullibayessets.py
+++ bayessets/test/test_bernoullibayessets.py
@@ -1,3 +1,7 @@
+"""Unit tests for the Bernoulli Bayesian Set algorithm
+"""
+
+
 from bayessets import BayesianSet
 import numpy as np
 
@@ -11,6 +15,21 @@ def test_hyperparameters_from_mean():
                      [0, 1, 0, 1]])
     means = np.array([0, 0.5, 0.25, 1])
     scale = 2
+    alpha, beta = BayesianSet.estimate_hyperparameters(scale, data)
+    assert np.all(alpha == (scale * means))
+    assert np.all(beta == scale - alpha)
+
+
+def test_hyperparameters_from_mean_unusual_scale():
+    """Tests the calculation of hyper-parameters alpha and beta,
+    not using the default C=2 constant
+    """
+    data = np.array([[0, 0, 0, 1],
+                     [0, 1, 0, 1],
+                     [0, 0, 1, 1],
+                     [0, 1, 0, 1]])
+    means = np.array([0, 0.5, 0.25, 1])
+    scale = 15
     alpha, beta = BayesianSet.estimate_hyperparameters(scale, data)
     assert np.all(alpha == (scale * means))
     assert np.all(beta == scale - alpha)
